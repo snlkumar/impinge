e08fcdeb6994a74ec9ca9965dcab9b7347f660dc
Wesley Moore <wes@trikeapps.com>
2013-01-19 20:44:38 +1100

    CRUD for Image

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,53 @@
+class ImagesController < ApplicationController
+
+  before_filter :find_image, :except => [:index, :new, :create]
+
+  def index
+    @images = Image.parents.paginate(:page => params[:page], :per_page => 10, :order =>:created_at)
+  end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(params[:image])
+
+    if @image.save
+      flash[:success] = "Image created"
+      redirect_to images_path
+    else
+      flash[:error] = "Unable to save image"
+      render :new
+    end
+  end
+
+  # def show
+  # end
+
+  def edit
+  end
+
+  def update
+    if @image.update_attributes(params[:image])
+      flash[:success] = "Image updated"
+      redirect_to images_path
+    else
+      flash[:error] = "Unable to update image"
+      render :edit
+    end
+  end
+
+  def destroy
+    @image.destroy
+    flash[:success] = "Image deleted"
+    redirect_to images_path
+  end
+
+protected
+
+  def find_image
+    @image = Image.find(params[:id])
+  end
+
+end
